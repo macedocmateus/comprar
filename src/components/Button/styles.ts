88a80394d8c0120b94db270c9804b485f73d5008
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-05-25 15:26:05 -0300

    add button component

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#2c46b1',
+    height: 48,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 14,
+    fontWeight: 600,
+  },
+})
