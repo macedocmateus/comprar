c90a91380e34d59709a7890cc55975a47302f47a
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-05-26 08:46:19 -0300

    add clear button

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,4 +1,4 @@
-import { View, Image } from 'react-native'
+import { View, Image, TouchableOpacity, Text } from 'react-native'
 import { styles } from './styles'
 
 import { Button } from '@/components/Button'
@@ -6,6 +6,8 @@ import { Input } from '@/components/Input'
 import { Filter } from '@/components/Filter'
 
 import { FilterStatus } from '@/types/FilterStatus'
+
+const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE]
 
 export function Home() {
   return (
@@ -18,8 +20,15 @@ export function Home() {
       </View>
 
       <View style={styles.content}>
-        <Filter status={FilterStatus.DONE} isActive={true} />
-        <Filter status={FilterStatus.PENDING} isActive={false} />
+        <View style={styles.header}>
+          {FILTER_STATUS.map(status => (
+            <Filter key={status} status={status} isActive />
+          ))}
+
+          <TouchableOpacity style={styles.clearButton}>
+            <Text style={styles.clearText}>Limpar</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   )
